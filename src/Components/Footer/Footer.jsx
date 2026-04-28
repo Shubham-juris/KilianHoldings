97ceba6f0b2d77537f6b1fe95d1d77e98cf8a84d
JurisNexaIt <shubham.developerjuris@gmail.com>
2026-04-29 00:56:03 +0500

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -87,9 +87,7 @@ const Footer = () => {
               sx={{
                 mb: 3,
                 p: 2,
-                border: "1px solid #e0e0e0",
                 borderRadius: 3,
-                backgroundColor: "#f9fbfb",
               }}
             >
               {[
